6f115ac83fa9e77dcac1700f7befbd67fe4ff777
Trever <treverg2@gmail.com>
2016-11-04 16:01:14 -0500

    Messages template set up

--- Battleship/Messages.cs
+++ Battleship/Messages.cs
@@ -8,5 +8,14 @@ namespace Battleship
 {
     class Messages
     {
+        private String UserID1;
+        private String UserID2;
+        private String Message;
+        public Messages(String UserID1, String UserID2, String Message)
+        {
+            this.UserID1 = UserID1;
+            this.UserID2 = UserID2;
+            this.Message = Message;
+        }
     }
 }
