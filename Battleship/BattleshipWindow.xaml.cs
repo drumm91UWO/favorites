6aad1269dba1145b5482fcb974a7080375009cfd
drumm91 <drumm91@uwosh.edu>
2016-11-04 16:16:19 -0500

    Latest update

--- Battleship/BattleshipWindow.xaml.cs
+++ Battleship/BattleshipWindow.xaml.cs
@@ -29,5 +29,17 @@ namespace Battleship
             Window stats = new StatsWindow();
             stats.Show();
         }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            Window settings = new SettingsWindow();
+            settings.Show();
+        }
+
+        private void users_Click(object sender, RoutedEventArgs e)
+        {
+            Window users = new UserWindow();
+            users.Show();
+        }
     }
 }
