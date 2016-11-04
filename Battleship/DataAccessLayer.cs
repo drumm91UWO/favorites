1f3128fd1d01c25eeebd56703e033b4f9bd80133
Trever <treverg2@gmail.com>
2016-11-04 15:58:17 -0500

    Data access layer working

--- Battleship/DataAccessLayer.cs
+++ Battleship/DataAccessLayer.cs
@@ -8,5 +8,27 @@ namespace Battleship
 {
     class DataAccessLayer
     {
+        private String[,] GetLayout1()
+        {
+            String[,] happy = new String[3, 3];
+            return happy;
+        }
+
+        private String[,] GetLaout2()
+        {
+            String[,] happy = new String[3,3];
+            return happy;
+        }
+
+        private String[,] GetFriends()
+        {
+            String[,] happy = new String[3, 3];
+            return happy;
+        }
+
+        private bool IsOnline(String UserName)
+        {
+            return true;
+        }
     }
 }
